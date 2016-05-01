14196bfc6be7864c68107b38c7fec5964ce8b037
James Christopher Adduono <jc@adduono.com>
2016-05-01 03:28:04 -0400

    SELinux forced enforcing/permissive patch
    Change-Id: I008e16574dfb8e3c6f72d15eb0e8d52fdacfe3eb

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -771,9 +771,6 @@ out:
 	kfree(n);
 	kfree(t);
 
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enforcing = 1;
-#endif
 	if (!selinux_enforcing)
 		return 0;
 	return -EPERM;
@@ -1526,9 +1523,6 @@ out:
 	kfree(t);
 	kfree(n);
 
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enforcing = 1;
-#endif
 	if (!selinux_enforcing)
 		return 0;
 	return -EACCES;
@@ -1820,9 +1814,6 @@ static inline int convert_context_handle_invalid_context(struct context *context
 	char *s;
 	u32 len;
 
-#ifdef CONFIG_ALWAYS_ENFORCE
-	selinux_enforcing = 1;
-#endif
 	if (selinux_enforcing)
 		return -EINVAL;
 
