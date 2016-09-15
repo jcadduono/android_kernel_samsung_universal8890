4971883c31990158586df52062db0e6eb0335be4
James Christopher Adduono <jc@adduono.com>
2016-09-14 22:14:48 -0400

    NetHunterify build scripts
    Change-Id: If4ca3cb2cafd55b662c70a09865fb670eef137b4

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Stock Samsung kernel for Samsung Exynos 8890 devices build script by jcadduono
+# Kali NetHunter kernel for Samsung Exynos 8890 devices build script by jcadduono
 
 ################### BEFORE STARTING ################
 #
@@ -65,7 +65,7 @@ export CROSS_COMPILE=$TOOLCHAIN/bin/aarch64-linux-gnu-
 [ -x "${CROSS_COMPILE}gcc" ] ||
 ABORT "Unable to find gcc cross-compiler at location: ${CROSS_COMPILE}gcc"
 
-[ "$TARGET" ] || TARGET=samsung
+[ "$TARGET" ] || TARGET=nethunter
 [ "$1" ] && DEVICE=$1
 [ "$2" ] && VARIANT=$2
 [ "$DEVICE" ] || DEVICE=gracelte
