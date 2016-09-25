4190b5c04cf2b74e3141f43b30011762ee2fd054
James Christopher Adduono <jc@adduono.com>
2016-09-25 13:08:07 -0400

    Linux 3.18.37
    Change-Id: I1b851fecaa9a0bf1bf6fd7ef8a75b3be3239a8cb

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1703,6 +1703,20 @@ static struct ctl_table fs_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_doulongvec_minmax,
 	},
+	{
+		.procname	= "pipe-user-pages-hard",
+		.data		= &pipe_user_pages_hard,
+		.maxlen		= sizeof(pipe_user_pages_hard),
+		.mode		= 0644,
+		.proc_handler	= proc_doulongvec_minmax,
+	},
+	{
+		.procname	= "pipe-user-pages-soft",
+		.data		= &pipe_user_pages_soft,
+		.maxlen		= sizeof(pipe_user_pages_soft),
+		.mode		= 0644,
+		.proc_handler	= proc_doulongvec_minmax,
+	},
 	{ }
 };
 
